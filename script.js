2a7c03aa89de57a9ac77e418d33bb10bf021b5c3
Ivan Vlahov <ivan99vlahov@gmail.com>
2026-01-22 17:20:17 +0100

    designübung last version

--- script.js
+++
@@ -1,6 +0,0 @@
-// JavaScript for Visualization Exercise
-console.log("Script loaded");
-
-// Example: Simple visualization setup
-const container = document.getElementById('container');
-container.innerHTML = '<p>Hello, World! This is a placeholder for your visualization.</p>';
